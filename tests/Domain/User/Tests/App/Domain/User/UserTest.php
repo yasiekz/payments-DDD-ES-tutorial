22cfc1de79c90c8edededb6c957d8342a82a93f4
Jan Zięba <j.zieba2@tvn.pl>
2018-01-07 15:23:38 +0100

    tests

--- tests/Domain/User/Tests/App/Domain/User/UserTest.php
+++ tests/Domain/User/Tests/App/Domain/User/UserTest.php
@@ -14,10 +14,17 @@ use Ramsey\Uuid\Uuid;
 class UserTest extends TestCase
 {
     const NAME = 'John Doe';
+    const NEW_NAME = 'John Doe the second';
+
     const POSTCODE_VALUE = '32-436';
     const STREET_VALUE = 'street';
     const POST_VALUE = 'post';
     const CITY_VALUE = 'city';
+
+    const POSTCODE2_VALUE = '12-436';
+    const STREET2_VALUE = 'street2';
+    const POST2_VALUE = 'post2';
+    const CITY2_VALUE = 'city2';
 
     private $id;
 
@@ -38,6 +45,16 @@ class UserTest extends TestCase
         $this->assertEquals(self::POST_VALUE, $user->getPost());
         $this->assertEquals(self::POSTCODE_VALUE, $user->getPostCode());
         $this->assertEquals(self::STREET_VALUE, $user->getStreet());
+
+        $user->changeName(self::NEW_NAME);
+        $this->assertEquals(self::NEW_NAME, $user->getName());
+
+        $user->changeAddress($this->getSecondAddress());
+
+        $this->assertEquals(self::CITY2_VALUE, $user->getCity());
+        $this->assertEquals(self::POST2_VALUE, $user->getPost());
+        $this->assertEquals(self::POSTCODE2_VALUE, $user->getPostCode());
+        $this->assertEquals(self::STREET2_VALUE, $user->getStreet());
     }
 
     /**
@@ -51,6 +68,21 @@ class UserTest extends TestCase
         $address->expects($this->any())->method('getPost')->willReturn(self::POST_VALUE);
         $address->expects($this->any())->method('getPostCode')->willReturn(self::POSTCODE_VALUE);
         $address->expects($this->any())->method('getCity')->willReturn(self::CITY_VALUE);
+
+        return $address;
+    }
+
+    /**
+     * @return Address|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getSecondAddress()
+    {
+        /** @var \PHPUnit_Framework_MockObject_MockObject|Address $address */
+        $address = $this->getMockBuilder(Address::class)->disableOriginalConstructor()->getMock();
+        $address->expects($this->any())->method('getStreet')->willReturn(self::STREET2_VALUE);
+        $address->expects($this->any())->method('getPost')->willReturn(self::POST2_VALUE);
+        $address->expects($this->any())->method('getPostCode')->willReturn(self::POSTCODE2_VALUE);
+        $address->expects($this->any())->method('getCity')->willReturn(self::CITY2_VALUE);
 
         return $address;
     }
